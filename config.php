04b4f6bfadef8d8a6b56ab32cfb7dd859d723893
Damon V. Caskey <dc@caskeys.com>
2017-05-25 11:42:17 -0400

    Update config.php
    Bit of formating cleanup for github.

--- config.php
+++ config.php
@@ -20,22 +20,26 @@ abstract class DEFAULTS
 	
 	// Query options.
 	const 
-		SCROLLABLE 	= SQLSRV_CURSOR_FORWARD, // Cursor type.
+		SCROLLABLE 	= SQLSRV_CURSOR_FORWARD, 		// Cursor type.
 		SENDSTREAM 	= TRUE,					// Send whole parameter stream (TRUE), or send chunks (FALSE).
 		TIMEOUT		= 300;					// Query time out in seconds.	
 	
 	// Line options.
 	const 
-		FETCHTYPE	= SQLSRV_FETCH_ASSOC,	// Default row array fetch type.
-		ROW			= SQLSRV_SCROLL_NEXT,	// Row to access in a result set that uses a scrollable cursor.
+		FETCHTYPE	= SQLSRV_FETCH_ASSOC,			// Default row array fetch type.
+		ROW		= SQLSRV_SCROLL_NEXT,			// Row to access in a result set that uses a scrollable cursor.
 		OFFSET		= 0;					// Row to access if row is absolute or relative. 
 	
 	// Error handling.
 	const 
 		TRAP		= TRUE,					// Toggles error handling. If turned off, errors will be left to the standard PHP error handler.
 		DETAILS		= TRUE,					// Send all available details about error to log.
-		MAXLENGTH	= 1000,					// Max length for error_log (set in php.ini). Error details exceeding this limit are split into multiple submissions.
-		NEW_ID		= -1,					// Using this ID in upsert type quries ensures the databse will find no matches and creaate new record.
+		MAXLENGTH	= 1000;					// Max length for error_log (set in php.ini). Error details exceeding this limit are split into multiple submissions.
+	
+	// New IDs. Passing these as IDs in upsert type quries ensures 
+	// the databse engine will find no matches and create 
+	// a new record.
+	const 	NEW_ID		= -1,					
 		NEW_GUID	= '00000000-0000-0000-0000-000000000000';
 }
 ?>
